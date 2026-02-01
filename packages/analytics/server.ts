ea2c091896456b611eb284f70bcb062e54f64948
sina85 <naeemi0sina@gmail.com>
2026-01-31 20:27:47 -0500

    Refactor PostHog analytics initialization to use a lazy-loading pattern for improved performance and resource management

--- packages/analytics/server.ts
+++ packages/analytics/server.ts
@@ -2,10 +2,23 @@ import "server-only";
 import { PostHog } from "posthog-node";
 import { keys } from "./keys";
 
-export const analytics = new PostHog(keys().NEXT_PUBLIC_POSTHOG_KEY, {
-  host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+let _analytics: PostHog | null = null;
 
-  // Don't batch events and flush immediately - we're running in a serverless environment
-  flushAt: 1,
-  flushInterval: 0,
+function getAnalytics(): PostHog {
+  if (!_analytics) {
+    _analytics = new PostHog(keys().NEXT_PUBLIC_POSTHOG_KEY, {
+      host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+
+      // Don't batch events and flush immediately - we're running in a serverless environment
+      flushAt: 1,
+      flushInterval: 0,
+    });
+  }
+  return _analytics;
+}
+
+export const analytics = new Proxy({} as PostHog, {
+  get(_, prop) {
+    return getAnalytics()[prop as keyof PostHog];
+  },
 });
