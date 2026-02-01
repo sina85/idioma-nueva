cac2cb8ff156985da4cf735a42438866ea52736a
sina85 <naeemi0sina@gmail.com>
2026-02-01 15:47:07 -0500

    Update homepage greeting from "Hello World 3" to "Hello World 45"

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello World 3</h1>
+      <h1>Hello World 45</h1>
     </main>
   );
 }
